dd80835e39843a950758cb688c9d91692cc8b49e
AaronKolenda <kolenda.a@gmail.com>
2015-02-18 14:51:54 -0500

    passing all tests

--- public/index.js
+++ public/index.js
@@ -5,9 +5,32 @@
     the station at 23rd and Crystal Drive (which is ID "31011")
 */
 var localStation = function(callback) {
+    $.ajax({
 
+    // URL to retrieve
+    url: "/stations",
 
-}
+    // HTTP verb to use
+    method: "GET",
+
+    // Function to execute when complete (the callback parameter)
+    success: function(data) {
+
+      var station = _.find(data, function(station) {
+        if (station.id === "31011") {
+          console.log(data.id, data.name);
+          return true;
+        }
+      
+      });
+
+      callback(station);
+    }
+
+  });
+
+  }
+
 
 /*
   This function accepts a callback function as a parameter.
@@ -19,7 +42,26 @@ var localStation = function(callback) {
 */
 var northernmostStation = function(callback) {
 
-  
+  $.ajax({
+
+    // URL to retrieve
+    url: "/stations",
+
+    // HTTP verb to use
+    method: "GET",
+
+    // Function to execute when complete (the callback parameter)
+    success: function(data) {
+
+      var station = _.max(data, function(station){ return station.latitude; });
+      //return station;
+
+      callback(station);
+
+      }
+
+    });
+
 }
 
 /*
@@ -29,6 +71,26 @@ var northernmostStation = function(callback) {
     a single, random station in the Capital Bikeshare system.
 */
 var randomStation = function(callback) {
+
+    $.ajax({
+
+    // URL to retrieve
+    url: "/stations",
+
+    // HTTP verb to use
+    method: "GET",
+
+    // Function to execute when complete (the callback parameter)
+    success: function(data) {
+
+      var station = _.sample(data);
+      //return station;
+
+      callback(station);
+
+      }
+
+    });
 
   
 }
@@ -42,6 +104,28 @@ var randomStation = function(callback) {
 */
 var emptyStations = function(callback) {
 
+  $.ajax({
+
+    // URL to retrieve
+    url: "/stations",
+
+    // HTTP verb to use
+    method: "GET",
+
+    // Function to execute when complete (the callback parameter)
+    success: function(data) {
+
+      var station = _.filter(data, function(station){
+          if (station.bikes === 0) {
+            return true;
+          }
+      });
+
+      callback(station);
+
+      }
+
+    });
   
 }
 
@@ -53,6 +137,29 @@ var emptyStations = function(callback) {
     in the last 15 minutes.
 */
 var recentStations = function(callback) {
+
+    $.ajax({
+
+    // URL to retrieve
+    url: "/stations",
+
+    // HTTP verb to use
+    method: "GET",
+
+    // Function to execute when complete (the callback parameter)
+    success: function(data) {
+
+      var station = _.filter(data, function(station){
+          if (station.lastUpdate > 1424225700000) {
+            return true;
+          }
+      });
+
+      callback(station);
+
+      }
+
+    });
 
   
 }
